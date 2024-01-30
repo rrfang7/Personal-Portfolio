ce875853aa8d018ef0abe96fdf0a9f2fe265f9ce
Ralph Fang <ralphrainierfang@yahoo.com>
2024-01-30 14:43:46 -0500

    Updated Experience style and put contains

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
+import { IoIosSchool } from "react-icons/io";
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <div className='experience'>
+    <VerticalTimeline lineColor='#3e497a'>
+      <VerticalTimelineElement 
+        className='vertical-timeline-element--education'
+        date='2009 - 2013'
+        iconStyle={{ background: "#3e497a", color: "#fff"}}
+        icon= {<IoIosSchool/>}>
+        <h3 className="vertical-timeline-element-title"> Greenpark High School, Philippines</h3>
+        <p> High School Diploma </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement 
+        className='vertical-timeline-element--education'
+        date='2009 - 2013'
+        iconStyle={{ background: "#3e497a", color: "#fff"}}
+        icon= {<IoIosSchool/>}>
+        <h3 className="vertical-timeline-element-title"> AMA Computer College, Philippines</h3>
+        <p> Unfinished Bachelor's of Science Degree </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement 
+        className='vertical-timeline-element--education'
+        date='2009 - 2013'
+        iconStyle={{ background: "#3e497a", color: "#fff"}}
+        icon= {<IoIosSchool/>}>
+        <h3 className="vertical-timeline-element-title"> Full Stack Academy, New York, USA</h3>
+        <p> Full Stack Developer Certificate </p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+    </div>
   )
 }
 
