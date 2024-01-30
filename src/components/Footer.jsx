be86b8d500f45ff0b26f58cf5c0ac50caefd6a17
Ralph Fang <ralphrainierfang@yahoo.com>
2024-01-30 17:31:42 -0500

    Updated icons and added job Experience.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./Footer.css"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 function Footer() {
   return (
     <div className='footer'>
-        <div> </div>
+        <div className='socialMedia'> 
+        <FaLinkedin />
+        <FaGithub />
+        <MdOutlineEmail />
+        </div>
         <p> &copy; 2024 rrfang.com </p>
     </div>
   )
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
 import "react-vertical-timeline-component/style.min.css"
 import { IoIosSchool } from "react-icons/io";
+import { MdOutlineWorkOutline } from "react-icons/md";
 
 function Experience() {
   return (
@@ -17,7 +18,7 @@ function Experience() {
       </VerticalTimelineElement>
       <VerticalTimelineElement 
         className='vertical-timeline-element--education'
-        date='2009 - 2013'
+        date='2019 - 2021'
         iconStyle={{ background: "#3e497a", color: "#fff"}}
         icon= {<IoIosSchool/>}>
         <h3 className="vertical-timeline-element-title"> AMA Computer College, Philippines</h3>
@@ -25,12 +26,26 @@ function Experience() {
       </VerticalTimelineElement>
       <VerticalTimelineElement 
         className='vertical-timeline-element--education'
-        date='2009 - 2013'
+        date='Sept 2023 - Dec 2023'
         iconStyle={{ background: "#3e497a", color: "#fff"}}
         icon= {<IoIosSchool/>}>
         <h3 className="vertical-timeline-element-title"> Full Stack Academy, New York, USA</h3>
         <p> Full Stack Developer Certificate </p>
       </VerticalTimelineElement>
+      <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2015 - 2019"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<MdOutlineWorkOutline />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            United States Army 
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Fort Stewart, GA
+          </h4>
+          <p>Served in the Army as a Diesel Mechanic</p>
+        </VerticalTimelineElement>
     </VerticalTimeline>
     </div>
   )
