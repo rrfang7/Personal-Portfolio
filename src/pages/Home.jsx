2d958f0e218553198121f1d9011379dbacf4d984
Ralph Fang <ralphrainierfang@yahoo.com>
2024-01-30 12:21:39 -0500

    Home page styling and front end render

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import "./Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='about'> 
+          <h2>Hi, my name is Ralph</h2>
+        <div className='prompt'>
+          <p>A software developer with a passion for learning and creating</p>
+        </div>
+        </div>
+        <div className='skills'> 
+          <h1>Skills</h1>
+          <ol className='list'>
+            <li className='item'>
+                <h2>Front-End</h2>
+                <span>ReactJS, HTML, CSS, NPM </span>
+            </li>
+            <li className='item'>
+                <h2>Back-End</h2>
+                <span>NodeJS, ExpressJS, SQL </span>
+            </li>
+            <li className='item'>
+                <h2>Languages</h2>
+                <span>JavaScript </span>
+            </li>
+          </ol>
+        </div>
+    </div>
   )
 }
 
