a6f9a613a882bdf6e83f9475618934d58b8455ad
Ralph Fang <ralphrainierfang@yahoo.com>
2024-02-02 10:50:07 -0500

    Added Projects image

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
+import proj1 from "../assets/proj1.png"
+import proj2 from "../assets/proj2.png"
+import "./Project.css"
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1> My Personal Projects</h1>
+      <div className='projectList'>
+      <ProjectItem name ="Calculator" image = {proj1}/>
+      <ProjectItem name="E-commerce" image = {proj2}/>
+      </div>
+    </div>
   )
 }
 
