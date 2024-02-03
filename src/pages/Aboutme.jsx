030150aacc76393e13b050433a524d0968c63544
Ralph Fang <ralphrainierfang@yahoo.com>
2024-02-03 18:44:42 -0500

    Added photos on Aboutme.jsx

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
-
+import hiking from "../assets/hiking.png"
+import hiking2 from "../assets/hiking2.png"
+import "./Aboutme.css"
 
 function Aboutme() {
   return (
-    <div>Hobbies</div>
+    <div>
+      <h2>About Me</h2>
+      <p>Hobbies:</p>
+      <img className="hiking" src={hiking} alt="hike" />
+      <img className="views" src={hiking2} alt="views" />
+    </div>
   )
 }
 
