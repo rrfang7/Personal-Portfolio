ee320bf079056a9e85e069c2b618a42a50e1473e
Ralph Fang <ralphrainierfang@yahoo.com>
2024-02-04 20:06:16 -0500

    Added paragraphs

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -28,13 +28,17 @@ function Aboutme() {
       <h2>About Me</h2>
       <p>Hobbies:</p>
       <img className="hiking" src={hiking} alt="hike" />
+      <p>One of the things I love to do are hiking with my wife and dog name Daisy.</p>
       <img className="views" src={hiking2} alt="views" />
+      <p>These are some of the hiking places I have taken pictures of in Ohio.</p>
+
 
       {/* Video element with play and stop buttons */}
       <video ref={videoRef} width="300" height="200" controls>
         <source src={boxing} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <p>My main exercise is boxing. I am also a big boxing fan! </p>
 
     </div>
   );
